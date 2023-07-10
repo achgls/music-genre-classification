f852169f390295d0e0e3a91291b2f5a98f41df6a
Achille <achille.gellens@gmail.com>
2023-07-10 11:15:27 +0200

    remove debugging printouts

--- src/train.py
+++ src/train.py
@@ -242,17 +242,13 @@ def main(ns_args):
 
     wav_aug = None
     if (wav_aug_kwargs := ns_args.wav_aug) is not None:
-        print(wav_aug_kwargs)
         wav_aug_kwargs = utils.parse_kwargs_arguments(wav_aug_kwargs)
-        print(wav_aug_kwargs, type(wav_aug_kwargs))
         print("Apply data augmentation of waveform with following parameters:", wav_aug_kwargs)
         wav_aug = WaveformAugment(**wav_aug_kwargs).to(device)
     spec_aug = None
     if (spec_aug_kwargs := ns_args.spec_aug) is not None:
-        print(spec_aug_kwargs)
         spec_aug_kwargs = utils.parse_kwargs_arguments(spec_aug_kwargs)
-        print(wav_aug_kwargs, type(wav_aug_kwargs))
-        print("Apply data augmentation of waveform with following parameters:", wav_aug_kwargs)
+        print("Apply data augmentation of spectrogram with following parameters:", spec_aug_kwargs)
         spec_aug = SpecAugment(**spec_aug_kwargs).to(device)
 
     loss_kwargs = utils.parse_kwargs_arguments(ns_args.loss_kwargs)
