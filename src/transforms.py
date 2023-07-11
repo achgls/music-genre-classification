e61cbe00af779eb3f2a7325f8651c8b2c9f8196a
Achille <achille.gellens@gmail.com>
2023-07-11 03:46:29 +0200

    fix typo

--- src/transforms.py
+++ src/transforms.py
@@ -48,7 +48,7 @@ def powerspec(log=False, normalize=False, norm_axis=None):
     if log:
         transform.add_module("Log", Log1p())
     if normalize:
-        transform.add_module("Std-Mean-Scaling", StdMeanScaling(norm_axis=norm_axis))
+        transform.add_module("Std-Mean-Scaling", StdMeanScaling(axis=norm_axis))
     transform.add_module("Power-Spectrogram", tt.Spectrogram(n_fft=1024, win_length=1024, hop_length=512, power=2.0))
     return transform
 
